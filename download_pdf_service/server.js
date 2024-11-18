01ad9f8f54447456f084e0a07e54649f4189be58
John Gallagher <john@synapticmishap.co.uk>
2024-11-18 06:59:25 +0000

    fix: server can now convert FCJSON to PDF

--- download_pdf_service/server.js
+++ download_pdf_service/server.js
@@ -4,12 +4,22 @@ const fs = require('fs').promises;
 const app = express();
 const port = 3004;
 
-app.get('/download_pdf', async (req, res) => {
+app.use(express.json());
+
+app.post('/download_pdf', async (req, res) => {
+  if (!req.body || !req.body.pattern) {
+    return res.status(400).json({ error: 'Pattern is required', body: req.body });
+  }
+
   const browserOptions = {
     wsEndpoint: 'ws://chrome:3000/playwright/chromium?token=6R0W53R135510'
   };
 
   try {
+    // Create temp file for the JSON data
+    const jsonTempPath = '/tmp/temp.fcjson';
+    await fs.writeFile(jsonTempPath, JSON.stringify(req.body.pattern));
+
     const browser = await chromium.connect(browserOptions.wsEndpoint);
     const context = await browser.newContext({
       acceptDownloads: true,
@@ -22,26 +32,35 @@ app.get('/download_pdf', async (req, res) => {
     await page.goto('https://flosscross.com/designer/');
     await page.waitForLoadState('networkidle');
 
+    console.log('Setting input files');
     const fileInput = await page.locator('input[type="file"]').first();
-    await fileInput.setInputFiles('data/uploads/Puffins.fcjson');
+    await fileInput.setInputFiles(jsonTempPath);
     await page.waitForLoadState('networkidle');
 
+    console.log('Navigating to https://flosscross.com/designer/slot/1/pdf');
     await page.goto('https://flosscross.com/designer/slot/1/pdf');
     await page.waitForLoadState('networkidle');
 
+    console.log('Clicking Save To PDF');
     await page.waitForSelector('button:has-text("Save To PDF")');
     await page.click('button:has-text("Save To PDF")');
 
+    console.log('Clicking OK');
     await page.waitForSelector('.q-btn__content:has-text("OK")');
     const downloadPromise = page.waitForEvent('download');
     await page.click('.q-btn__content:has-text("OK")');
 
     const download = await downloadPromise;
-    const tempPath = '/tmp/temp.pdf';
-    await download.saveAs(tempPath);
+    console.log('Saving to PDF');
+    const pdfTempPath = '/tmp/temp.pdf';
+    await download.saveAs(pdfTempPath);
 
-    const pdfBuffer = await fs.readFile(tempPath);
-    await fs.unlink(tempPath); // Clean up temp file
+    const pdfBuffer = await fs.readFile(pdfTempPath);
+    
+    console.log('Cleaning up temp files');
+    // Clean up temp files
+    await fs.unlink(pdfTempPath);
+    await fs.unlink(jsonTempPath);
 
     res.contentType('application/pdf');
     res.send(pdfBuffer);
@@ -55,5 +74,5 @@ app.get('/download_pdf', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`PDF service listening at the http://localhost:${port}`);
+  console.log(`PDF service listening at V2 http://localhost:${port}`);
 }); 
