0d56320d256ae05d066028ef34e53596cc7e5b45
John Gallagher <john@synapticmishap.co.uk>
2025-02-22 12:50:33 +0000

    fix: allow replit to show web app

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module MiniCrossStitching
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.action_dispatch.default_headers = {
+      "X-Frame-Options" => "ALLOWFROM replit.com"
+    }
   end
 end
